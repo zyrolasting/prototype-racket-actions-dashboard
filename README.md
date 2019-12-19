87b73b2fef4237333de2ebe6328e777c3da6dcea
Sage Gerard <sage@sagegerard.com>
2019-12-19 13:35:47 -0500

    Minimal server

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 prototype-racket-actions-dashboard
 ==================================
-README text here.
+
+This is an early stage exploration into a portal for Racket build
+reviews and monitoring. Specific requirements are not yet defined,
+and the implementation is as unstable as it could possibly be.
+
+* `records.rkt-list` holds a Racket list in the format `((ID . HASH))`, where `ID` is an exact non-negative integer and `HASH` is a Racket hash literal.
+* `/build/123` will simply display a formatted document of record with ID `123` from `records.rkt-list`.
--- main.rkt
+++ main.rkt
@@ -1,50 +1,65 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require net/url
+         racket/dict
+         racket/file
+         racket/format
+         racket/function
+         racket/list
+         racket/runtime-path
+         setup/getinfo
+         web-server/web-server
+         web-server/http
+         web-server/dispatchers/dispatch
+         [prefix-in lifter: web-server/dispatchers/dispatch-lift]
+         [prefix-in sequencer: web-server/dispatchers/dispatch-sequencer])
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(define-runtime-path here ".")
 
-;; Code here
-
-
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
+(define (show-build-record records req)
+  (let* ([path/params (url-path (request-uri req))]
+         [id (and (> (length path/params) 0)
+                  (string->number (path/param-path (first path/params))))])
+    (response/xexpr
+     `(html (head (title ,(format "Record: ~a" id)))
+            (body (pre ,(if (dict-has-key? records id)
+                            (~e (dict-ref records id))
+                            (format "Record ~a not found" id))))))))
 
 (module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
   (require racket/cmdline)
-  (define who (box "world"))
+
+  (define port (box 8080))
   (command-line
-    #:program "my-program"
+    #:program "racket-ci-dashboard"
     #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
+    [("-p" "--port")
+     user-port
+     "Port on which to listen for connections"
+     (let ([parsed (string->number user-port)])
+           (if (exact-positive-integer? parsed)
+               (set-box! port parsed)
+               (begin
+                 (printf "The port doesn't look right: ~v~n" parsed)
+                 (exit 1))))]
+    #:args () (void))
+
+  (define records (file->value (build-path here
+                                           "records.rkt-list")))
+
+  (define show-build-record/loaded
+    (curry show-build-record records))
+
+  (writeln records)
+
+  (define stop (serve #:dispatch (lifter:make show-build-record/loaded)
+                      #:port (unbox port)))
+
+  (printf "CI Dashboard v~a listening on http://[::1]:~a~n"
+          ((get-info/full here) 'version)
+          (unbox port))
+
+  (with-handlers ([exn:break? (λ _
+                                (displayln "User break: Shutting down")
+                                (stop))])
+    (sync/enable-break never-evt)))
